efed48ba2cde8e0d2620148f2537fc1722de8918
krimdata <easystatistique@gmail.com>
2025-04-03 15:56:02 +0200

    nouveau dep

--- variables.tf
+++ variables.tf
@@ -15,6 +15,6 @@ variable "app_service_plan_name" {
 
 variable "webapp_name" {
   description = "Nom de l'Azure Web App"
-  default     = "my-neweeee-free-webappppppp"
+  default     = "my-neweeee-free-webappppp"
 }
 
